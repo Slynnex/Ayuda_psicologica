5d2dbba97927b05e8a357d02eb809be0200c44e5
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-01-11 15:16:59 -0600

    Ventana de Plan de seguridad

---
+++ app/src/main/java/com/example/ayuda_psicologica/Activity_PDS.java
@@ -0,0 +1,14 @@
+package com.example.ayuda_psicologica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_PDS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__p_d_s);
+    }
+}
