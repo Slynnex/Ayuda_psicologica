1dbe108544bbdfd8eb32a240f42498f3d0140c1c
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-01-11 15:00:42 -0600

    Ventana de menu principal

---
+++ app/src/main/java/com/example/ayuda_psicologica/Menuprincipal.java
@@ -0,0 +1,14 @@
+package com.example.ayuda_psicologica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menuprincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menuprincipal);
+    }
+}
